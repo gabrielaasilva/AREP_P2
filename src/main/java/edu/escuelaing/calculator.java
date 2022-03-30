5145d94e38c959e3b38e8f79a2fd1954107811b8
unknown <ana.silva-b@mail.escuelaing.edu.co>
2022-03-30 13:35:24 -0500

    Funciones

--- src/main/java/edu/escuelaing/calculator.java
+++ src/main/java/edu/escuelaing/calculator.java
@@ -5,11 +5,23 @@ import org.json.JSONObject;
 import spark.Request;
 
 
-public class calculator
-{
+public class calculator {
+
     public static void main( String[] args )
     {
 
+    }
+
+    public static Double tan(String value){
+        Double ans1 = 0.0;
+        ans1 = Math.tan(Double.parseDouble(value));
+        return ans1;
+    }
+
+    public static Double acos(String value){
+        Double ans2 = 0.0;
+        ans2 = Math.acos(Double.parseDouble(value));
+        return ans2;
     }
 
     private static int getPort() {
